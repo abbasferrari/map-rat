b9a2aafdd2be7b6651b4efadefd37abf2dd8b83c
abbasferrari <abbas.ferrari@gmail.com>
2018-09-13 12:00:46 +0530

    Add files via upload

---
+++ updateGeojson.php
@@ -0,0 +1,18 @@
+<?php
+$changeDensity = $_REQUEST["density"];
+$ofCity = $_REQUEST["name"];
+
+header('Content-type: application/json');
+$strJsonFileContents = file_get_contents("neighborhood.geojson");
+$arr = json_decode($strJsonFileContents,true);
+
+for($i = 0 ; $i < count($arr["features"]); $i++){
+if ( strcmp($ofCity,$arr["features"]["".$i.""]["properties"]["Name"]) == 0) {
+   $arr["features"]["".$i.""]["properties"]["density"] = $changeDensity;
+   echo json_encode($arr["features"]["".$i.""]["properties"]["Name"]);
+   break;
+}
+}
+$newJsonString = json_encode($arr);
+file_put_contents('neighborhood.geojson',$newJsonString);
+?>
